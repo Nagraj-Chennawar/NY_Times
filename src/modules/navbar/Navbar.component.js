be7ddfa02c94683c395ab4229cd8cd977c7ce456
Nagraj4443 <nagrajchennawar15@gmail.com>
2024-07-19 13:45:03 +0530

    Router setup and basic components added

---
+++ src/modules/navbar/Navbar.component.js
@@ -0,0 +1,25 @@
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import { MenuItem } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+function Navbar() {
+    const navigate = useNavigate();
+    const handleHomeClick = () => navigate('/');
+
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <MenuItem key={"home"} onClick={handleHomeClick}>
+            <Typography textAlign="center">Home</Typography>
+          </MenuItem>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
+
+export default Navbar;
---
+++ src/modules/not-found/NotFound.component.js
@@ -0,0 +1,10 @@
+import { Alert } from '@mui/material'
+import React from 'react'
+
+function NotFound() {
+  return (
+    <Alert severity="error">Rsource you are looking for is not found</Alert>
+  )
+}
+
+export default NotFound
