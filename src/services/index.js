d8459b9927e0cd589adbc55a10d7c72d20bf83a3
cms1234s <91181974+cms1234s@users.noreply.github.com>
2024-08-06 10:57:23 +0530

    console added

--- src/services/index.js
+++ src/services/index.js
@@ -10,6 +10,7 @@ class ApiService {
   async get(url, params = {}) {
     try {
       const response = await this.api.get(url, { params });
+      console.log("response", response);
       return { error: null, data: response.data };
     } catch (error) {
       this.callBack();
